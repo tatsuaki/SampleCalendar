6ad180850b57ab097e2704f1a0cfbb84cf9a12b8
tatsuaki_kin <tatuaki_dear@yahoo.co.jp>
2017-10-21 19:16:54 +0900

    init3

---
+++ app/src/main/java/com/tatuaki/k/test/calendar/samplecalendar/MainActivity.java
@@ -0,0 +1,56 @@
+package com.tatuaki.k.test.calendar.samplecalendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+        setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.button1);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu");
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, "onClick");
+        if (v == mButton) {
+            Log.d(TAG, "click mButton");
+        }
+    }
+}
